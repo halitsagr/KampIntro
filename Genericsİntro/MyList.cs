0dc07443b0e24e8099ead6dfeaf2f5637779d174
halit sağır <halitsagir2@gmail.com>
2021-01-20 23:50:52 +0300

    20 Ocak Ders Sonu

---
+++ Genericsİntro/MyList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Genericsİntro
+{
+    class MyList<T> // T yerine istenilen yazılabilir.
+    {
+
+        T[] items;
+
+
+        //constructor
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)   // item yerine istenilen yazılabilir.
+        {
+            T[] tempArray = items;  //geciciDizi
+            items = new T[items.Length+1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }  
+
+    }
+}
---
+++ Genericsİntro/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Genericsİntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Engin");
+        }
+    }
+}
